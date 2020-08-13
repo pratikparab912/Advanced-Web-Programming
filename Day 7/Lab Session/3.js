ff74fa6e47554a69cc6cdf41355797ef16d88695
pratikparab912 <60909504+pratikparab912@users.noreply.github.com>
2020-08-13 21:51:26 +0530

    Add files via upload

---
+++ Day 7/Lab Session/3.js
@@ -0,0 +1,10 @@
+function compute() {
+    var x = Number(document.getElementById('num1').value);
+    var y = Number(document.getElementById('num2').value);
+    var z = 0;
+
+    if (document.getElementById('add')) {
+        z = x + y;
+    }
+    document.getElementById('ans').value = z;
+}
